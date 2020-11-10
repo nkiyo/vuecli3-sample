1be90223ef7a1832aa0f81f1a670184c48863e79
nkiyo <nkiyo@hoge.com>
2020-11-11 07:31:56 +0900

    Update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ reversedMessage }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+      Forrr a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
@@ -38,6 +38,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String,
+  },
+  computed: {
+    reversedMessage() {
+      return this.msg.split('').reverse().join();
+    },
   },
 };
 </script>
