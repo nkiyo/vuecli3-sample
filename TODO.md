52bacc89ac125d72041ef50c7e298eb487692d28
nkiyo <nkiyo@hoge.com>
2020-11-17 08:57:02 +0900

    Modify vue.config.js

---
+++ TODO.md
@@ -0,0 +1,7 @@
+- __done__ MPA
+  - 参考
+    - https://qiita.com/kamicop/items/c5c0917ed226234ee288
+    - https://cli.vuejs.org/config/#pages
+- MPAの各画面からコンポーネントをimportして使う
+- webpack reverse proxy
+
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   pages: {
     main: {
-      entry: 'src/pages/top/main.js',
+      entry: 'src/pages/main/main.js',
       template: 'public/index.html',
       filename: 'index.html',
+      title: 'Main Page',
     },
     sub1: {
       entry: 'src/pages/sub1/main.js',
-      template: 'public/sub1.html',
-      filename: 'sub1.html',
+      title: 'Sub1',
     },
+    // 設定方法は、https://cli.vuejs.org/config/#pages を参考にした
   },
 };
