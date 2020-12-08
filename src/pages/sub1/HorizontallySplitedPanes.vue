232870c36fc7bb0f1bace0b3a9be4e1000fd2b05
nkiyo <nkiyo@hoge.com>
2020-12-09 08:00:11 +0900

    Update

---
+++ src/pages/sub1/HorizontallySplitedPanes.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <slot name="left">left undefined</slot>
+    <slot name="right">right undefined</slot>
+  </div>
+</template>
+<script>
+</script>
--- src/pages/sub1/Sub1.vue
+++ src/pages/sub1/Sub1.vue
@@ -1,13 +1,39 @@
 <template>
+  <HorizontallySplitedPanes>
+    <template slot="left">
+      <ul>
+        <li>left</li>
+        <li>left</li>
+        <li>left</li>
+        <li>left</li>
+        <li>left</li>
+        <li>left</li>
+      </ul>
+    </template>
+    <template slot="right">
+      <ul>
+        <li>right</li>
+        <li>right</li>
+        <li>right</li>
+        <li>right</li>
+        <li>right</li>
+        <li>right</li>
+      </ul>
+    </template>
+  </HorizontallySplitedPanes>
+  <!--
   <Main/>
+  -->
 </template>
 
 <script>
-import Main from '@/pages/sub1/Main.vue';
+// import Main from '@/pages/sub1/Main.vue';
+import HorizontallySplitedPanes from '@/pages/sub1/HorizontallySplitedPanes.vue';
 
 export default {
   components: {
-    Main,
+    // Main,
+    HorizontallySplitedPanes,
   },
   methods: {
   },
